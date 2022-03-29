e85b4c30a051b2e4f020bfd14820f29f7637bc43
Jakub Sedláček <Sedlacek.Jakub@stud.vassboskovice.cz>
2022-03-29 11:36:13 +0200

    Přidejte soubory projektu.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace game
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Warrior warrior1;
+        Warrior warrior2;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            warrior1 = new Warrior("Patrik");
+            warrior2 = new Warrior("Kuba");
+            txt1.Text = warrior1.ToString();           
+            txt2.Text = warrior2.ToString();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WpfApp1/Warrior.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace game
+{
+     public class Warrior
+     {
+        private int shield;
+        private int health;
+        private int damage;
+
+        public string Name { get; }
+        public int Health { get=>health; }
+        public int Shield { get=>shield; }
+        public int Damage { get=> damage; }
+
+        public Warrior(string name)
+        {
+            Name = name;
+            health = 100;
+            shield = 50;
+            damage = 20;
+        }
+
+        public void GetHit(int damage)
+        {        
+            shield -= damage;
+            if(shield < 0)
+            {
+                health -= shield;
+                shield = 0;
+            }
+           
+        }
+        public override string ToString()
+        {
+            string text = "Name " + Name + "\n";
+            text += "Health " + Health + "\n";
+            text += "Shield " + Shield + "\n";
+            text += "Damage " + Damage + "\n";
+            return text;
+        }
+    }
+}
